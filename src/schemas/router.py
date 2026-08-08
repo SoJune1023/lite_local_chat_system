bb94563fe1470d326a8ce42824b52c5fa9aee956
SoJune1023 <ssojune@naver.com>
2026-08-08 19:10:31 +0900

    feat: Add claude client

--- src/schemas/router.py
+++ src/schemas/router.py
@@ -1,7 +1,9 @@
+from typing import Literal
 from pydantic import BaseModel
 
 class InteractionRequest(BaseModel):
     message: str
+    model: Literal['claude', 'ollama']
 
 class InteractionResponse(BaseModel):
     message: str
